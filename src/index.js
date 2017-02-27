8678b9dd69a9816144c80672c59d11ed30c65f39
Pavel Egorov <snake7799@users.noreply.github.com>
2017-02-27 22:20:50 +0300

    closure/feature: implement sum method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function sum() {
-  // your solution
+    let summa = 0;
+    
+    for(let key in arguments)
+        summa += arguments[key];
+    
+    let ret = sum.bind(null, summa);
+
+    ret.valueOf = () => summa;
+    
+    return ret;
 }
